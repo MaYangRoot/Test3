5ef68e938097df966b025ac1d2f60ac05216a0f7
www <13633835270@qq.com>
2015-11-11 20:05:28 +0800

    bbb

---
+++ bvmmmn/bbbbbViewController.m
@@ -0,0 +1,38 @@
+//
+//  bbbbbViewController.m
+//  bvmmmn
+//
+//  Created by qianfeng on 15/11/11.
+//  Copyright © 2015年 马扬. All rights reserved.
+//
+
+#import "bbbbbViewController.h"
+
+@interface bbbbbViewController ()
+
+@end
+
+@implementation bbbbbViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor orangeColor];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
