a0c3c6c15ad968fc569c9e4e43a2cf7f96819049
wheng <wanghengbai@126.com>
2015-11-11 20:17:48 +0800

    modified by wheng third

--- bvmmmn/AppDelegate.m
+++ bvmmmn/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "WHClassA_classAExtension.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    [[WHClassA shareManager] bark];
+    
     return YES;
 }
 
